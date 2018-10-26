da55e13317b4c97728f36e149719ffb31b55438a
EkaterinaMalasay <ekaterina.malasay@gmail.com>
2018-10-26 10:32:16 +0300

    hello module with parameters

--- hello.c
+++ hello.c
@@ -1,20 +1,27 @@
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+
+static char *whom = "world";
+module_param(whom, charp, 0);
+
+static int howmany = 1;
+module_param(howmany, int, 0);
 
 static int __init hello_init(void)
 {
-	pr_alert("Good morrow to this fair assembly.\n");
+	int i;
+
+	for (i = 0; i < howmany; i++)
+		pr_alert("(%d) Hello, %s\n", i, whom);
 	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-	pr_alert("Alas, poor world, what treasure hast thou lost!\n");
+	pr_alert("Goodbye, cruel %s\n", whom);
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Greeting module");
-MODULE_AUTHOR("William Shakespeare");
